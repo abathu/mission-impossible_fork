cb4d7526a81c414635f65bcd614d044267fc5202
abathu <2573257955@qq.com>
2025-06-04 06:09:36 +0800

    Create windows version
    change the file path and create a .bahs instead of the .sh file for runing on windows

--- training/prepare_training.sh
+++ training/prepare_training.sh
@@ -1,63 +1,60 @@
-#!/bin/sh
-# prepare_training.sh
-# author: Julie Kallini
+@echo off
+REM prepare_training.bat
+REM author: Julie Kallini
 
-readonly MISTRAL_PATH=/exports/eddie/scratch/s2678328/mistral
+setlocal
 
-echo "
--------------------------------------------------------------------------------
-Arguments
--------------------------------------------------------------------------------
-"
-echo "Perturbation type: $1"
-echo "Train set: $2"
-echo "Random seed: $3"
-echo "Paren pretrained model: $4"
-NO_POS_ENCODINGS=${5:-''}
-echo "No pos encodings: $NO_POS_ENCODINGS"
-echo "Mistral path: $MISTRAL_PATH"
+set "MISTRAL_PATH=C:\Users\abathur\Desktop\MscProject\mistral"
 
-if [ -z "$NO_POS_ENCODINGS" ]
-then
-    NPS=""
-    NPSunderscore=""
-else
-    NPS="-no-positional-encodings"
-    NPSunderscore="_no_positional_encodings"
-fi
+echo.
+echo ------------------------------------------------------------------------------
+echo Arguments
+echo ------------------------------------------------------------------------------
+echo Perturbation type: %1
+echo Train set: %2
+echo Random seed: %3
+echo Parent pretrained model: %4
 
-# Generate yaml files for mistral training
-echo "
--------------------------------------------------------------------------------
-Generating yaml files for mistral training
--------------------------------------------------------------------------------
-"
-GENERATE_YAML_COMMAND="python3 generate_yaml.py $1 $2 $3 $4 $NO_POS_ENCODINGS"
-echo $GENERATE_YAML_COMMAND
-eval $GENERATE_YAML_COMMAND
+set "NO_POS_ENCODINGS=%5"
+if "%NO_POS_ENCODINGS%"=="" (
+    set "NPS="
+    set "NPSunderscore="
+) else (
+    set "NPS=-no-positional-encodings"
+    set "NPSunderscore=_no_positional_encodings"
+)
 
-# Copy yaml files to mistral directory
-echo "
--------------------------------------------------------------------------------
-Copying config yaml files to mistral directory
--------------------------------------------------------------------------------
-"
-COPY_DATASET_COMMAND="cp conf/babylm_$1_$2_$4$NPSunderscore/seed$3/dataset_$1_$2_seed$3.yaml $MISTRAL_PATH/conf/datasets/dataset_$1_$2_seed$3.yaml"
-echo $COPY_DATASET_COMMAND
-eval $COPY_DATASET_COMMAND
-echo ""
+echo No pos encodings: %NO_POS_ENCODINGS%
+echo Mistral path: %MISTRAL_PATH%
 
-COPY_TRAIN_COMMAND="cp conf/babylm_$1_$2_$4$NPSunderscore/seed$3/train_$1_$2_$4$NPSunderscore"_"seed$3.yaml $MISTRAL_PATH/conf/train_$1_$2_$4$NPSunderscore"_"seed$3.yaml"
-echo $COPY_TRAIN_COMMAND
-eval $COPY_TRAIN_COMMAND
-echo ""
+echo.
+echo ------------------------------------------------------------------------------
+echo Generating yaml files for mistral training
+echo ------------------------------------------------------------------------------
+echo python generate_yaml.py %1 %2 %3 %4 %NO_POS_ENCODINGS%
+python generate_yaml.py %1 %2 %3 %4 %NO_POS_ENCODINGS%
 
-COPY_MODEL_COMMAND="cp conf/babylm_$1_$2_$4$NPSunderscore/gpt2$NPS-small-$1-$4.yaml $MISTRAL_PATH/conf/models/gpt2$NPS-small-$1-$4.yaml"
-echo $COPY_MODEL_COMMAND
-eval $COPY_MODEL_COMMAND
+echo.
+echo ------------------------------------------------------------------------------
+echo Copying config yaml files to mistral directory
+echo ------------------------------------------------------------------------------
+set "SRC1=conf\babylm_%1_%2_%4%NPSunderscore%\seed%3\dataset_%1_%2_seed%3.yaml"
+set "DST1=%MISTRAL_PATH%\conf\datasets\dataset_%1_%2_seed%3.yaml"
+echo copy "%SRC1%" "%DST1%"
+copy "%SRC1%" "%DST1%"
 
-echo "
--------------------------------------------------------------------------------
-Done!
--------------------------------------------------------------------------------
-"
+set "SRC2=conf\babylm_%1_%2_%4%NPSunderscore%\seed%3\train_%1_%2_%4%NPSunderscore%_seed%3.yaml"
+set "DST2=%MISTRAL_PATH%\conf\train_%1_%2_%4%NPSunderscore%_seed%3.yaml"
+echo copy "%SRC2%" "%DST2%"
+copy "%SRC2%" "%DST2%"
+
+set "SRC3=conf\babylm_%1_%2_%4%NPSunderscore%\gpt2%NPS%-small-%1-%4.yaml"
+set "DST3=%MISTRAL_PATH%\conf\models\gpt2%NPS%-small-%1-%4.yaml"
+echo copy "%SRC3%" "%DST3%"
+copy "%SRC3%" "%DST3%"
+
+echo.
+echo ------------------------------------------------------------------------------
+echo Done!
+echo ------------------------------------------------------------------------------
+endlocal
